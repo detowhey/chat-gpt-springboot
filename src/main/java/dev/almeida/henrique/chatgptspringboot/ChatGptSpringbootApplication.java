bb95e1ede54d2ba988259476f96db5db6aab584a
Henrique de Freitas Almeida <56054329+detowhey@users.noreply.github.com>
2023-11-18 18:51:29 -0300

    First files commit

---
+++ src/main/java/dev/almeida/henrique/chatgptspringboot/ChatGptSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.almeida.henrique.chatgptspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/almeida/henrique/chatgptspringboot/ChatGptSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.almeida.henrique.chatgptspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
