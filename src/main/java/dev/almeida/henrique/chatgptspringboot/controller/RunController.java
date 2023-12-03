2d59ef67a8ab916afbf735ca0e7319c604f57d20
Henrique de Freitas Almeida <56054329+detowhey@users.noreply.github.com>
2023-12-03 19:56:01 -0300

    Refactor ResourceExceptionHandler.java

--- src/main/java/dev/almeida/henrique/chatgptspringboot/controller/RunController.java
+++ src/main/java/dev/almeida/henrique/chatgptspringboot/controller/RunController.java
@@ -1,15 +1,12 @@
 package dev.almeida.henrique.chatgptspringboot.controller;
 
 import com.theokanning.openai.runs.Run;
-import com.theokanning.openai.runs.RunStep;
 import dev.almeida.henrique.chatgptspringboot.service.RunService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/${api.version}/bot", produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/dev/almeida/henrique/chatgptspringboot/exception/handler/ResourceExceptionHandler.java
+++ src/main/java/dev/almeida/henrique/chatgptspringboot/exception/handler/ResourceExceptionHandler.java
@@ -1,10 +1,12 @@
 package dev.almeida.henrique.chatgptspringboot.exception.handler;
 
+import dev.almeida.henrique.chatgptspringboot.exception.*;
 import dev.almeida.henrique.chatgptspringboot.exception.error.StandardErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -13,6 +15,46 @@ import java.time.Instant;
 @Slf4j
 @RestControllerAdvice
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(AssistantNotFoundException.class)
+    public ResponseEntity<StandardErrorResponse> assistantNotFound(
+            AssistantNotFoundException exception,
+            ServletServerHttpRequest request
+    ) {
+        return createResponseError(HttpStatus.NOT_FOUND, "Not found", exception, request);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<StandardErrorResponse> fileNotFound(
+            FileNotFoundException exception,
+            ServletServerHttpRequest request
+    ) {
+        return createResponseError(HttpStatus.NOT_FOUND, "Not found", exception, request);
+    }
+
+    @ExceptionHandler(MessaUserNotFoundException.class)
+    public ResponseEntity<StandardErrorResponse> messageNotFound(
+            MessaUserNotFoundException exception,
+            ServletServerHttpRequest request
+    ) {
+        return createResponseError(HttpStatus.NOT_FOUND, "Not found", exception, request);
+    }
+
+    @ExceptionHandler(RunNotFoundException.class)
+    public ResponseEntity<StandardErrorResponse> runNotFound(
+            RunNotFoundException exception,
+            ServletServerHttpRequest request
+    ) {
+        return createResponseError(HttpStatus.NOT_FOUND, "Not found", exception, request);
+    }
+
+    @ExceptionHandler(ThreadBotNotFoundException.class)
+    public ResponseEntity<StandardErrorResponse> threadBotNotFound(
+            ThreadBotNotFoundException exception,
+            ServletServerHttpRequest request
+    ) {
+        return createResponseError(HttpStatus.NOT_FOUND, "Not found", exception, request);
+    }
 
     private ResponseEntity<StandardErrorResponse> createResponseError(HttpStatus httpStatus, String error, Exception exception, ServletServerHttpRequest request) {
         StandardErrorResponse errorResponse = new StandardErrorResponse(
@@ -26,4 +68,3 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(httpStatus).body(errorResponse);
     }
 }
-
