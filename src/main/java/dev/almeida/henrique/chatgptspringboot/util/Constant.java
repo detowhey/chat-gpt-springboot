f0a4d96a8ff7a28d8a689eba3c94494ba0a0061c
detowhey <henriquefr.almeida@gmail.com>
2024-05-11 15:03:53 -0300

    refactor: retire Constant from .gitignore file

---
+++ src/main/java/dev/almeida/henrique/chatgptspringboot/util/Constant.java
@@ -0,0 +1,7 @@
+package dev.almeida.henrique.chatgptspringboot.util;
+
+public interface Constant {
+
+    String OPENAPI_TOKEN = PropertiesResource.getProperty("chatKey");
+    String ASSISTANT_ID = PropertiesResource.getProperty("assistantKey");
+}
